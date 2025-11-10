4e37f6b4115a334a5d6d8646cfc3c30cd8158d98
Poojii001 <poojapal5781@gmail.com>
2025-11-10 23:43:16 +0530

    ReactJS

---
+++ src/Components/Test.jsx
@@ -0,0 +1,10 @@
+//class component
+import { Component } from "react";
+class Test extends Component {
+    render() {
+        return (
+            <h1>This is Class Components</h1>
+        );
+    }
+}
+export default Test;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Test from './Test.jsx';
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+     <Test/>
+  </StrictMode>
+)
