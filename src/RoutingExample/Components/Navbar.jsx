73f99dd04420802c743f9679d10acbc23661afb3
Poojii001 <poojapal5781@gmail.com>
2025-11-18 14:10:27 +0530

    Redirecting to home page

--- src/RoutingExample/Components/Navbar.jsx
+++ src/RoutingExample/Components/Navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar() {
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/profile/Pooja%20Pal/Trainer/134000">Profile</NavLink></li>
         <li><NavLink to="/contact?name=Pooja%20Pal&email=poojapal@gmail.com&phone=9506580566">Contact</NavLink></li>
+        {/* <li><NavLink to="/Error">Error</NavLink></li> */}
     </ul>
    </>
   )
--- src/RoutingExample/Pages/ErrorPage.jsx
+++ src/RoutingExample/Pages/ErrorPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function ErrorPage() {
+  let [num, setNum] = useState(5)
+  let navigate = useNavigate()
+
+  useEffect(() => {
+    let time = setTimeout(() => {
+      if (num === 0)
+        navigate("/")
+      setNum(num - 1)
+    }, 1000);
+    return () => clearTimeout(time)
+  }, [num])
+
   return (
     <>
-        <h1>404! Page not found</h1>
+      <h1>404! Page not found</h1>
+      <h2>Redirect to the HomePage in {num} seconds</h2>
     </>
   )
 }
