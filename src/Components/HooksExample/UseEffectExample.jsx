e038c6b1d1c2b9008e3c6a9402b60b937f561350
Poojii001 <poojapal5781@gmail.com>
2025-12-07 09:52:42 +0530

    HooksExample

--- src/Components/HooksExample/UseEffectExample.jsx
+++ src/Components/HooksExample/UseEffectExample.jsx
@@ -1,3 +1,37 @@
+// import React, { useState, useEffect } from 'react'
+
+// export default function UseEffectExample() {
+//   let [num1, setNum1] = useState(1)
+//   let [num2, setNum2] = useState(1)
+//   let [num3, setNum3] = useState(1)
+
+//   //useEffect without dependency , called setup function every time when component is rendered.
+//   useEffect(()=>{
+//     console.log("useEffect without dependency is called")
+//   })
+
+//   //useEffect with empty dependency array , called setup function one one time ,when component is mounted.
+//   useEffect(()=> {
+//     console.log("useEffect with empty dependency array is called")
+//   },[])
+
+//   //useEffect with  dependency, called setup function every time ,when dependecy is changed.
+//   useEffect(()=> {
+//     console.log("useEffect with empty dependency array is called")
+//   },[num2])
+//   return (
+//       <>
+//         <h1>useEffect Example</h1>
+//         <h1>num1 = {num1} | num2 = {num2} | num3 = {num3}</h1>
+//         <button onClick = {()=>setNum1(num1+1)}>Inc Num1</button>
+//         <button onClick = {()=>setNum2(num2+1)}>Inc Num2</button>
+//         <button onClick = {()=>setNum3(num3+1)}>Inc Num3</button>
+//       </>
+//   )
+// }
+
+
+
 import React, { useState, useEffect } from 'react'
 
 export default function UseEffectExample() {
@@ -5,20 +39,18 @@ export default function UseEffectExample() {
   let [num2, setNum2] = useState(1)
   let [num3, setNum3] = useState(1)
 
-  //useEffect without dependency , called setup function every time when component is rendered.
+  let count = 1
+
   useEffect(()=>{
-    console.log("useEffect without dependency is called")
+    count = 1
+    let time = setInterval(()=>{
+      console.log(count)
+      count++
+      if(count === 30)
+        alert("session Timeout")
+    },1000)
+    return()=>clearInterval(time) //cleanup func
   })
-
-  //useEffect with empty dependency array , called setup function one one time ,when component is mounted.
-  useEffect(()=> {
-    console.log("useEffect with empty dependency array is called")
-  },[])
-
-  //useEffect with  dependency, called setup function every time ,when dependecy is changed.
-  useEffect(()=> {
-    console.log("useEffect with empty dependency array is called")
-  },[num2])
   return (
       <>
         <h1>useEffect Example</h1>
@@ -29,4 +61,6 @@ export default function UseEffectExample() {
       </>
   )
 }
+
+
 
---
+++ src/Components/HooksExample/useMemoExample.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import React, { useState, useMemo } from 'react'
+
+function calculation(num) {
+    for(let i = 1; i <= 50000000; i++)
+    {
+        sum = sum +1
+    }
+    return sum
+}
+
+
+export default function useMemoExample() {
+    let [num1, setNum1] = useState(1)
+    let [num2, setNum2] = useState(1)
+    let [num3, setNum3] = useState(1)
+
+
+
+    //useEffect without dependency , called setup function every time when component is rendered.
+    useEffect(() => {
+        console.log("useEffect without dependency is called")
+    })
+
+    //useEffect with empty dependency array , called setup function one one time ,when component is mounted.
+    useEffect(() => {
+        console.log("useEffect with empty dependency array is called")
+    }, [])
+
+    //useEffect with  dependency, called setup function every time ,when dependecy is changed.
+    useEffect(() => {
+        console.log("useEffect with empty dependency array is called")
+    }, [num2])
+
+    return (
+
+        <>
+            <h1>useEffect Example</h1>
+            <h1>num1 = {num1} | num2 = {num2} | num3 = {num3}</h1>
+            <button onClick={() => setNum1(num1 + 1)}>Inc Num1</button>
+            <button onClick={() => setNum2(num2 + 1)}>Inc Num2</button>
+            <button onClick={() => setNum3(num3 + 1)}>Inc Num3</button>
+        </>
+
+
+    )
+}
+
+
+
