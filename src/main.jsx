776713a4a2d34bd57608042e55e01d155db514da
Poojii001 <poojapal5781@gmail.com>
2025-12-07 10:14:48 +0530

    HooksExample

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Parent from './Components/HooksExample/Parent'
+ import useRExample from './Components/HooksExample/useRefExample'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <Parent/>
+    
+    <useRefExample/>
       
   </StrictMode>,
 )
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import Parent from './Components/HooksExample/Parent'
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//       <Parent/>
+
+//   </StrictMode>,
+// )
 
 
 // import { StrictMode } from 'react'
