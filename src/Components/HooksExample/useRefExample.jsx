1faa9ef65941f84a100ca02c2fa499612e0a0eca
Poojii001 <poojapal5781@gmail.com>
2025-12-07 22:12:27 +0530

    HooksExample

--- src/Components/HooksExample/useRefExample.jsx
+++ src/Components/HooksExample/useRefExample.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function useRefExample() {
+  let [name, setName] = useState("")
+  let email = useRef("")
+  console.log("Component is Rendered")
+
+
+  function postData(){
+    alert(`
+        Name    :  ${name}
+        Email    :  ${email}
+      `)
+  }
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>useEffect Example</h1>
+      <h1>num1 = {num1} | num2 = {num2} | num3 = {num3}</h1>
+      <button onClick={() => setNum1(num1 + 1)}>Inc Num1</button>
+      <button onClick={() => setNum2(num2 + 1)}>Inc Num2</button>
+      <button onClick={() => setNum3(num3 + 1)}>Inc Num3</button>
+    </>
+
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
- import useRExample from './Components/HooksExample/useRefExample'
 
+import useRefExample from './Components/HooksExample/useRefExample'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+
     <useRefExample/>
       
   </StrictMode>,
